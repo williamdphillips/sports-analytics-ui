1adfac804b9d13786474ae9c7142ede9957800dd
williamdphillips <william.phillips21@outlook.com>
2021-10-21 00:29:45 -0500

    Small change to use single compiled js file

--- scripts/model.js
+++ scripts/model.js
@@ -1,7 +1,3 @@
-// Sports Analytics
-let baseURL = 'http://localhost:8080';
-var resp = '';
-
 function updateGames(games) {
     console.log(games);
     let container = document.getElementById("middleContentContainer");
@@ -47,47 +43,4 @@ function updateGames(games) {
     }
 
     container.innerHTML = htmlGames.join('');
-}
-
-var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.response);
-        }
-        
-        anHttpRequest.open( "GET", aUrl, true );
-        anHttpRequest.responseType = 'json';
-        anHttpRequest.send();
-    }
-}
-
-function updateDocument(endpoint, document, params) {
-
-    console.log(baseURL);
-    let searchURL = new URL(`${baseURL}${endpoint}`);
-
-    let searchParams = new URLSearchParams(searchURL.searchParams);
-    for (const [key, value] of Object.entries(params)) {
-        searchParams.append(key, value);
-    }
-    searchURL.search = searchParams;
-    console.log('GET Triggered');
-    console.log(searchURL.href);
-
-    var client = new HttpClient();
-    client.get(searchURL, function(response) {
-      updateResponse(response, document);
-});
-}
-
-function updateResponse(response, document){
-    if(document.name == 'GamesByWeek')
-        updateGames(response);
-}
-
-function getGamesByWeek(week) {
-    console.log(`Getting Games for week ${week}`)
-    updateDocument('/api/nfl/simple/games', {name:'GamesByWeek'}, { week: week });
 }
