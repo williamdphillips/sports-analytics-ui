50210413db97b8636a1ea5586b2072f869fc0b3d
williamdphillips <william.phillips21@outlook.com>
2022-08-06 21:12:23 -0500

    Support for preseason prediction based upon moneyline

--- js/scripts.js
+++ js/scripts.js
@@ -14,17 +14,17 @@ function setInnerHtml(elm, html) {
 function getCardModel(game, i, accordionStatus) {
     if (game.state == "pre" || game.state == "post") {
         return `<div class="col">
-        <div class="card text-center">
-    <div class="card-body">
-        <div class="card-left" href="#">
-            <img src="${game.awayTeam.logoURL}" width="40" height="40" alt="">
-            <br>
-            <a href="#" class="card-anchor">${game.awayTeam.shortName}</a>
-            <a href="#" class="card-anchor" style="margin-top: -0.3rem; font-size: 0.8rem;">(${game.awayTeam.record})</a>
-            <img class="card-football-left"
+    <div class="card text-center">
+        <div class="card-body">
+            <div class="card-left" href="#">
+                <img src="${game.awayTeam.logoURL}" width="40" height="40" alt="">
+                <br>
+                <a href="#" class="card-anchor">${game.awayTeam.shortName}</a>
+                <a href="#" class="card-anchor" style="margin-top: -0.3rem; font-size: 0.8rem;">(${game.awayTeam.record})</a>
+                <img class="card-football-left"
                     src="images/toppng.com-american-football-ball-png-vector-600x368.png" width="19"
                     height="12" alt="" style="${game.currentPlay.awayTeamPossession}">
-        </div>
+            </div>
         <div class="card-center" href="#">
             <p class="card-bold">${game.awayTeam.currentScore}-${game.homeTeam.currentScore}</p>
             <p class="card-center-small">
@@ -48,99 +48,98 @@ function getCardModel(game, i, accordionStatus) {
         </div>
         <div id="collapse${i}" class="panel-collapse collapse in ${accordionStatus}">
             <div class="panel-body">
-            <div style="display:block; width=100%;">
-            
-                <div style="width: 11rem; margin:auto;">
-                <canvas id="winPercentage${i}"></canvas>
-                <script>
-                var data = {
-                    labels: ['${game.homeTeam.abbrev}', '${game.awayTeam.abbrev}'],
-                    datasets: [{
-                        data: [${game.awayTeam.winPrediction}, ${game.homeTeam.winPrediction}],
-                        backgroundColor: [
-                            '#${game.homeTeam.primaryColor}',
-                            '#${game.awayTeam.primaryColor}'
-                        ],
-                        hoverOffset: 4
-                    }]
-                };
-            
-                var config = {
-                    type: 'doughnut',
-                    data: data,
-                    options: {
-                        plugins: {
-                            title: {
-                                display: true,
-                                text: 'Win Probability'
-                            },
-                            legend: {
-                                display: true,
-                                reverse: true
+                <div style="display:block; width=100%;">
+                    <div style="width: 11rem; margin:auto;">
+                        <canvas id="winPercentage${i}"></canvas>
+                        <script>
+                        var data = {
+                            labels: ['${game.homeTeam.abbrev}', '${game.awayTeam.abbrev}'],
+                            datasets: [{
+                                data: [${calculateWinPercentage(game.awayTeam.winPrediction, game.homeTeam.moneyLine)}, ${calculateWinPercentage(game.homeTeam.winPrediction, game.awayTeam.moneyLine)}],
+                                backgroundColor: [
+                                    '#${game.homeTeam.primaryColor}',
+                                    '#${game.awayTeam.primaryColor}'
+                                ],
+                                hoverOffset: 4
+                            }]
+                        };
+        
+                        var config = {
+                            type: 'doughnut',
+                            data: data,
+                            options: {
+                                plugins: {
+                                    title: {
+                                        display: true,
+                                        text: 'Win Probability'
+                                    },
+                                    legend: {
+                                        display: true,
+                                        reverse: true
+                                    }
+                                }
                             }
-                        }
-                    }
-                };
-            
-                var myChart = new Chart(
-                    document.getElementById('winPercentage${i}'),
-                    config
-                );
-                </script>
-                </div>
-                                </div>
-                                <div style="float:left; display: inline-block; margin-top:-5rem;">
-                                    <table style="text-align: right; font-size:.6rem;">
-                                        <tr>
-                                            <td>Moneyline:</td>
-                                            <td>${game.awayTeam.moneyLine}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Spread Odds:</td>
-                                            <td>${game.awayTeam.spreadOdds}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Spread Rec:</td>
-                                            <td>${game.awayTeam.spreadSummary}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Avg Score:</td>
-                                            <td>${game.awayTeam.averageScore}</td>
-                                        </tr>
-                                    </table>
-                                </div>
-                                <div style="float:right; display: inline-block; margin-top:-5rem;">
-                                    <table style="text-align:right;font-size:.6rem;">
-                                        <tr>
-                                            <td>Moneyline:</td>
-                                            <td>${game.homeTeam.moneyLine}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Spread Odds:</td>
-                                            <td>${game.homeTeam.spreadOdds}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Spread Rec:</td>
-                                            <td>${game.homeTeam.spreadSummary}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Avg Score:</td>
-                                            <td>${game.homeTeam.averageScore}</td>
-                                        </tr>
-                                    </table>
-                                </div>
-                                <div
-                                    style="font-size:.6rem; display: block; margin: auto;">
-                                    Over / Under | Spread<br>${game.overUnder} | ${game.spreadDetails}
-                                </div>
-            </div>
+                        };
+                    
+                        var myChart = new Chart(
+                            document.getElementById('winPercentage${i}'),
+                            config
+                        );
+                        </script>
+                        </div>
+                    </div>
+                        <div style="float:left; display: inline-block; margin-top:-5rem;">
+                            <table style="text-align: right; font-size:.6rem;">
+                                <tr>
+                                    <td>Moneyline:</td>
+                                    <td>${game.awayTeam.moneyLine}</td>
+                                </tr>
+                                <tr>
+                                    <td>Spread Odds:</td>
+                                    <td>${game.awayTeam.spreadOdds}</td>
+                                </tr>
+                                <tr>
+                                    <td>Spread Rec:</td>
+                                    <td>${game.awayTeam.spreadSummary}</td>
+                                </tr>
+                                <tr>
+                                    <td>Avg Score:</td>
+                                    <td>${game.awayTeam.averageScore}</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div style="float:right; display: inline-block; margin-top:-5rem;">
+                            <table style="text-align:right;font-size:.6rem;">
+                                <tr>
+                                    <td>Moneyline:</td>
+                                    <td>${game.homeTeam.moneyLine}</td>
+                                </tr>
+                                <tr>
+                                    <td>Spread Odds:</td>
+                                    <td>${game.homeTeam.spreadOdds}</td>
+                                </tr>
+                                <tr>
+                                    <td>Spread Rec:</td>
+                                    <td>${game.homeTeam.spreadSummary}</td>
+                                </tr>
+                                <tr>
+                                    <td>Avg Score:</td>
+                                    <td>${game.homeTeam.averageScore}</td>
+                                </tr>
+                            </table>
+                        </div>
+                            <div
+                                style="font-size:.6rem; display: block; margin: auto;">
+                                Over / Under | Spread<br>${game.overUnder} | ${game.spreadDetails}
+                            </div>
         </div>
     </div>
+</div>
 </div>
 </div>`;
     }
     else {
-        return `<div class="col">
+            return `<div class="col">
         <div class="card text-center">
     <div class="card-body">
         <div class="card-left" href="#">
@@ -195,6 +194,17 @@ function getCardModel(game, i, accordionStatus) {
     </div>
 </div>
 </div>`;
+    }
+}
+
+function calculateWinPercentage(prediction, moneyLine){
+    if(prediction !== 0)
+        return prediction;
+
+    if(moneyLine < 0){
+        return moneyLine/(moneyLine-100)
+    }else{
+        return moneyLine/(moneyLine+100)
     }
 }
 
@@ -259,6 +269,7 @@ function updateGameCards(response) {
             if (game.currentPlay.driveDescription == null || game.currentPlay.driveDescription == undefined)
                 game.currentPlay.driveDescription = "";
 
+            //football icon --------------------------------------------------------------------------
             game.currentPlay.awayTeamPossession = "visibility: hidden;"
             game.currentPlay.homeTeamPossession = "visibility: hidden;"
 
